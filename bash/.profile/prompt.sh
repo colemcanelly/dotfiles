98d01ec276565dc3e7971e1101ca175585ca3b99
Cole McAnelly <76577549+colemcanelly@users.noreply.github.com>
2024-06-17 12:42:36 -0500

    Refactor prompt.sh to be more concise and support Root prompt

--- bash/.profile/prompt.sh
+++ bash/.profile/prompt.sh
@@ -1,27 +1,12 @@
 #!/usr/bin/env bash
 
-# myps1
-# (Mike Kasberg PS1)
-# (Or, Make PS1)
-
-# Modified by Cole McAnelly
-
-# Different functions generate different parts (segments) of the PS1 prompt.
-# Each function should leave the colors in a clean state (e.g. call reset if they changed any colors).
+# Cole McAnelly
 
 # For Git PS1
-source /usr/lib/git-core/git-sh-prompt;
+# source /usr/lib/git-core/git-sh-prompt;
+source ~/.bash_git;
 # GIT_PS1_SHOWDIRTYSTATE=1
 # GIT_PS1_SHOWUPSTREAM="verbose"
-
-# BLUE=`tput setaf 6`;
-BLUE=`tput setaf 032`;
-
-__myps1_debian_chroot() {
-    # This string is intentionally single-quoted:
-    # It will be evaluated when $PS1 is evaluated to generate the prompt each time.
-    echo '${debian_chroot:+($debian_chroot)}';
-}
 
 __myps1_inject_exitcode() {
     local code=$1
@@ -30,84 +15,51 @@ __myps1_inject_exitcode() {
     if [ "$code" -ne "0" ]; then
         local red=`tput setaf 1`;
         local ex="✗";
-        echo "${red} ${ex} ${reset}"
+        echo "${red} ${ex}"
     else
         local green=`tput setaf 34`;
         local check="✓";
-        echo "${green} ${check} ${reset}"
+        echo "${green} ${check}"
     fi
 }
 
-__myps1_exitcode() {
-    # local bg_red=`tput setab 1`;
-    local BG_GRAY=`tput setab 240`;
-    local white=`tput setaf 15`;
-    local reset=`tput sgr0`;
-
-    # We need to run a function at runtime to evaluate the exitcode.
-    echo "\[${BG_GRAY}\]\$(__myps1_inject_exitcode \$?)\[${reset}\]"
-}
-
-__myps1_time() {
-    local BG_GRAY=`tput setab 240`;
-    local white=`tput setaf 7`;
-    local reset=`tput sgr0`;
-
-    echo "\[${BG_GRAY}${white}\] \t \[${reset}\]"
-}
-
-__myps1_username() {
-    local reset=`tput sgr0`;
-
-    echo "\[${BLUE}\] \u \[${reset}\]";
-}
-
-__myps1_arrow() {
-    local bold=`tput bold`;
-    local reset=`tput sgr0`;
-
-    echo "➜ \[${reset}\]";
-}
-
-__myps1_workdir() {
-    local bold=`tput bold`;
-    local reset=`tput sgr0`;
-
-    # \w = Full path
-    # \W = Working Dir
-
-    echo " [\[${BLUE}\]\W\[${reset}\]]";
-}
-
-__myps1_git() {
-    local magenta=`tput setaf 213`;
-    local reset=`tput sgr0`;
-
-    # Escaping the $ is intentional:
-    # This is evaluated when the prompt is generated.
-    echo "\$(__git_ps1 ' (\[${magenta}\]%s\[${reset}\])')"
-}
-
-__myps1_box_top() {
-    local reset=`tput sgr0`;
-    echo "\n\[${BLUE}\]╭╴\[${reset}\]"
-}
-
-__myps1_box_bottom() {
-    local reset=`tput sgr0`;
-    echo "\n\[${BLUE}\]╰╴\[${reset}\]"
-}
-
-
-__myps1_user_prompt() {
-    local bold=`tput bold`;
+__myps1() {
     local reset=`tput sgr0`;
     
-    echo "\[${bold}\]\$\[${reset}\] ";
-}
+    # background
+    local bg_gray=`tput setab 240`;
+    
+    # text
+    local bold=`tput bold`;
+    local white=`tput setaf 7`;
+    local git_color=`tput setaf 213`;
+    local main_color=`tput setaf 032`;
 
-__myps1() {
-    local ps1="$(__myps1_box_top)$(__myps1_debian_chroot)$(__myps1_exitcode)$(__myps1_time)$(__myps1_username)$(__myps1_arrow)$(__myps1_workdir)$(__myps1_git)$(__myps1_box_bottom)$(__myps1_user_prompt)";
+    # symbols
+    local prompt_char="$";
+
+    if [ "`id -u`" -eq 0 ]; then    # root
+        prompt_char="#"
+        main_color=`tput setaf 1`;  # red
+        git_color=`tput setaf 214`; # gold
+    fi
+
+    local INIT='${debian_chroot:+($debian_chroot)}';
+    
+    # Symbols
+    local ARROW="➜\[${reset}\]";
+    local BOX_TOP="\n\[${main_color}\]╭╴\[${reset}\]";
+    local BOX_BOT="\n\[${main_color}\]╰╴\[${reset}\]";
+
+    # Information
+    local EXIT="\[${bg_gray}\]\$(__myps1_inject_exitcode \$?)";
+    local TIME="\[${white}\] \t \[${reset}\]"
+    local USER="\[${main_color}\]\u\[${reset}\]";
+    local DIR=" [\[${main_color}\]\W\[${reset}\]]";
+    local GIT="\$(__git_ps1 '(\[${git_color}\]%s\[${reset}\])')";
+    local PROMPT="\[${bold}\]${prompt_char}\[${reset}\] ";
+
+    local ps1="$BOX_TOP$(__myps1_debian_chroot)$EXIT $TIME $USER $ARROW $DIR $GIT $BOX_BOT$PROMPT";
 
     echo "$ps1";
 }
@@ -115,6 +67,6 @@ __myps1() {
 # To test, for example, print output before changes and after changes
 # and see if the diff is correct.
 # Uncomment for testing:
-#__myps1;
+# __myps1;
 
 PS1="$(__myps1)"; export PS1;
