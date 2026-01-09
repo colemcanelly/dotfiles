2d2139efce12072a2961f83040cf76f24eed78fa
Cole McAnelly <cole.mcanelly@valentpartners.com>
2026-01-09 14:35:20 -0600

    Added stack trace to errors.sh

--- shared/errors.sh
+++ shared/errors.sh
@@ -1,12 +1,27 @@
 src_dir=$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")
 source $src_dir/colors.sh
 
+
+function trace() {
+    for frame in "${BASH_SOURCE[@]:1}"; do
+        echo "    at ${frame}" >&2
+    done
+}
+
 # For use in shell-level functions
 #
 # # This will not exit the login shell, but will terminate the current script
 #
 function throw() {
-	echo "$(small)[error]$(reset)" "$@" >&2
+    local trace=false
+    local args=("$@");
+    if [[ " $@ " =~ " --trace " ]]; then
+        args=("${args[@]/--trace/}");
+        trace=true
+    fi
+    echo "$(small)[error]$(reset)" "${args[@]}" >&2
+    [ "$trace" = true ] && trace ;
+
 	kill -INT $$
 }
 
@@ -15,7 +30,16 @@ function throw() {
 #
 # > Warning: This will exit the entire shell if sourced in a login shell
 function panic() {
-	echo "$(small)[panic]$(reset)" "$@" >&2
+    local trace=false
+    local args=("$@");
+    if [[ " $@ " =~ " --trace " ]]; then
+        args=("${args[@]/--trace/}");
+        trace=true
+    fi
+
+	echo "$(small)[panic]$(reset)" "${args[@]}" >&2
+	[ "$trace" = true ] && trace ;
+
 	exit 1
 }
 
