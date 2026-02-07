70c9bf081fbcc12bea47329e7e5d2ac95011e350
Cole McAnelly <cole.mcanelly@valentpartners.com>
2026-02-07 13:07:14 -0600

    update for systems without command-not-found

--- shared/check_cmds.sh
+++ shared/check_cmds.sh
@@ -1,7 +1,17 @@
 
+
+not_found() {
+	if test -x /usr/lib/command-not-found; then
+		/usr/lib/command-not-found -- "$1"
+	else
+		echo "Command '$1' not found."
+	fi
+}
+
+
 function check_cmds() {
     for cmd in "$@"; do
         command -v $cmd &> /dev/null && continue;
-        /usr/lib/command-not-found -- "$cmd"
+        not_found "$cmd"
     done
 };
