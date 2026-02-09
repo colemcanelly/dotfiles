b849b424d4b817d2e87bde2c8965a7023d1b42cb
Cole McAnelly <cole.mcanelly@valentpartners.com>
2026-02-09 15:10:20 -0600

    add distro command and distro on tmux status bar

--- packages/zsh/.config/zsh/keybinds.zsh
+++ packages/zsh/.config/zsh/keybinds.zsh
@@ -3,3 +3,9 @@
 
 bindkey "^[[1;5C" emacs-forward-word
 bindkey "^[[1;5D" emacs-backward-word
+
+bindkey "\E[1~" beginning-of-line
+bindkey "\E[4~" end-of-line
+bindkey "\E[H" beginning-of-line
+bindkey "\E[F" end-of-line
+bindkey "\E[3~" delete-char
